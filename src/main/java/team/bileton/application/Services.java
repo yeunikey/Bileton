1e20f2773c6df6cfddec226dd4c16db35c9042d8
Yerassyl <erasylunerbekxd@gmail.com>
2025-01-21 10:46:21 +0600

    register new services

--- src/main/java/team/bileton/application/Services.java
+++ src/main/java/team/bileton/application/Services.java
@@ -1,6 +1,10 @@
 package team.bileton.application;
 
 import lombok.Getter;
+import team.bileton.application.models.Cinema;
+import team.bileton.application.services.CinemaService;
+import team.bileton.application.services.FilmService;
+import team.bileton.application.services.TicketService;
 import team.bileton.application.services.UserService;
 import team.bileton.application.services.interfaces.Service;
 
@@ -8,17 +12,20 @@ import team.bileton.application.services.interfaces.Service;
 public class Services {
 
     private final UserService userService = register(new UserService());
+    private final TicketService ticketService = register(new TicketService());
+    private final FilmService filmService = register(new FilmService());
+    private final CinemaService cinemaService = register(new CinemaService());
 
     public void setup() {
-        // todo add managers
+
     }
 
-    public <T extends Service<?>> T register(T service) {
+    public <T extends Service<?, ?>> T register(T service) {
         service.onLoad();
         return service;
     }
 
-    public <T extends Service<?>> T unregister(T service) {
+    public <T extends Service<?, ?>> T unregister(T service) {
         service.onUnload();
         return service;
     }
