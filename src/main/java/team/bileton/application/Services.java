494501f265e16714f6ab10cb373fd17ef7c8abda
Zaki <163705728+zaki100z@users.noreply.github.com>
2025-01-24 11:20:32 +0500

    odrder HistoryRepository was added

--- src/main/java/team/bileton/application/Services.java
+++ src/main/java/team/bileton/application/Services.java
@@ -10,24 +10,19 @@ import team.bileton.application.services.interfaces.Service;
 
 @Getter
 public class Services {
-
     private final UserService userService = register(new UserService());
     private final TicketService ticketService = register(new TicketService());
     private final FilmService filmService = register(new FilmService());
     private final CinemaService cinemaService = register(new CinemaService());
-
     public void setup() {
 
     }
-
     public <T extends Service<?, ?>> T register(T service) {
         service.onLoad();
         return service;
     }
-
     public <T extends Service<?, ?>> T unregister(T service) {
         service.onUnload();
         return service;
     }
-
 }
